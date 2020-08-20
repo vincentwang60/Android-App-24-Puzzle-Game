a84d026b67744ed95d9f81381b8663bd0fbd37b8
terrencewang <terrence.wang.98@gmail.com>
2020-08-20 16:37:22 -0700

    added reading from textfile image selection card drag and drop

--- app/src/main/java/edu/mit/vkwang/Card.java
+++ app/src/main/java/edu/mit/vkwang/Card.java
@@ -10,12 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Card extends AppCompatActivity{
     int x = 0;
     int y = 0;
+    boolean selected = false;
     ImageView card;
+
     public Card(ImageView gCard1,int resId){
         card = gCard1;
         card.setImageResource(resId);
+        card.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                selected = true;
+            }
+        });
     }
-    public void setTouchPos(int gX,int gY){
+    public void setTouchPos(int gX, int gY){
         x = gX;
         y = gY;
         card.setX(x);
--- app/src/main/java/edu/mit/vkwang/MainActivity.java
+++ app/src/main/java/edu/mit/vkwang/MainActivity.java
@@ -6,29 +6,61 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     int x = 0;
     int y = 0;
-    Card card;
+    Card[] cards;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView number2 = (ImageView) findViewById(R.id.card1);
-        card = new Card(number2,R.drawable.nines);
-        /*final TextView number2 = (TextView) findViewById(R.id.number2);
-        final TextView number3 = (TextView) findViewById(R.id.number3);
-        final TextView number4 = (TextView) findViewById(R.id.number4);*/
+        String string = "";
+        ArrayList<String> combos = new ArrayList<String>();
+        InputStream is = this.getResources().openRawResource(R.raw.combinations);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            try {
+                if ((string = reader.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            combos.add(string);
+        }
+
+        Collections.shuffle(combos);
+        String comboStr = combos.remove(0);
+        combos.add(comboStr);
+
+        String[] cardIDs = comboStr.split("\\s+");
+        String[] suits = new String[]{"s","c","d","h"};
+
+        for(int i = 0; i < cards.length; i++){
+            String suit = suits[(int)(Math.random() * suits.length)];
+            String cardID = cardIDs[i] + suit;
+            int resID = getResources().getIdentifier(cardID, "id", "package.name");
+            ImageView image = (ImageView) findViewById(resID);
+            cards[i] = new Card(image, resID);
+        }
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
         x = (int)event.getX();
         y = (int)event.getY();
-        card.setTouchPos(x,y);
+        for(Card card: cards){
+                if(card.selected){
+                    card.setTouchPos(x, y);
+                }
+        }
         //card.update();
         int[] offset = {-100,-150};
 
@@ -38,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
             case MotionEvent.ACTION_MOVE:
                 text.setText(Integer.toString(x)+','+Integer.toString(y));
             case MotionEvent.ACTION_UP:
+                for(Card card : cards){
+                    card.selected = false;
+                }
         }
 
         return false;
