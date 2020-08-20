8d9746133ba57b7256e86e528c2179b609647bfa
vincentkwang1 <45569552+vincentkwang1@users.noreply.github.com>
2020-08-20 10:51:29 -0700

    card imgs

--- app/src/main/java/edu/mit/vkwang/MainActivity.java
+++ app/src/main/java/edu/mit/vkwang/MainActivity.java
@@ -4,11 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,23 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView image = (ImageView) findViewById((R.id.image1));
+        card obj = new card();
+        final TextView number1 = (TextView) findViewById(R.id.number1);
+        /*final TextView number2 = (TextView) findViewById(R.id.number2);
+        final TextView number3 = (TextView) findViewById(R.id.number3);
+        final TextView number4 = (TextView) findViewById(R.id.number4);*/
     }
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
         int x = (int)event.getX();
         int y = (int)event.getY();
-        int[] offset = {-30,-250};
+        int[] offset = {-100,-150};
 
         TextView text = (TextView) findViewById(R.id.text1);
-        final ImageView image = (ImageView) findViewById((R.id.image1));
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
                 text.setText(Integer.toString(x)+','+Integer.toString(y));
-                image.setX((float) x+offset[0]);
-                image.setY((float) y+offset[1]);
             case MotionEvent.ACTION_UP:
         }
 
---
+++ app/src/main/java/edu/mit/vkwang/card.java
@@ -0,0 +1,14 @@
+package edu.mit.vkwang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class card extends AppCompatActivity{
+    public card(){
+        setContentView(R.layout.activity_main);
+        final TextView number2 = (TextView) findViewById(R.id.number2);
+    }
+}
